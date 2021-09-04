6c4536842f337eb3ab2d06b0fb59c8a4723c32ec
Skylar Grant <official.xian@gmail.com>
2021-09-04 17:10:18 -0400

    Bug squashing

--- dot-commands/spongebob.js
+++ dot-commands/spongebob.js
@@ -9,7 +9,7 @@ module.exports = {
 		let flipper = 0;
 		let newText = '';
 		for (const letter of commandData.args) {
-			if (letter == ' ') return;
+			if (letter == ' ') continue;
 			if (flipper == 0) {
 				newText = newText + letter.toUpperCase();
 				flipper = 1;
